f8812156065d54689ee1c99a69db15a235ebc3bd
Priyanshu Das <dasp98458@gmail.com>
2025-10-23 11:47:11 +0530

    Initial commit: Set up Node.js server and connect to MongoDB

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  timeout: 30000,
 });
 
 const sendWelcomeEmail = async (email, username) => {
